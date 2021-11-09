5f6979febba2ddb077e2265934028c75458bdc43
tanzeel <tanzeelwahid111@gmail.com>
2021-11-09 15:16:06 +0500

    hulu with next

---
+++ Components/HeaderItem.js
@@ -0,0 +1,10 @@
+const HeaderItem = ({title, Icon}) => {
+    return (
+        <div className="flex flex-col items-center cursor-pointer w-12 hover:text-white sm:w-20 group">
+            <Icon className="h-8 mb-1 group-hover:animate-bounce" />
+            <p className="tracking-widest opacity-0 group-hover:opacity-100"> {title} </p>
+        </div>
+    )
+}
+
+export default HeaderItem
